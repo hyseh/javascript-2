24dc03e6d65c4b4e5bcac9696d4fad569db6bd5d
hyseh <edgyhyseh@gmail.com>
2023-10-13 17:35:13 +0200

    added profiles endpoint

--- js/utility/api.js
+++ js/utility/api.js
@@ -2,3 +2,4 @@ export const BASE_URL = 'https://api.noroff.dev/api/v1';
 export const LOGIN_ENDPOINT = '/social/auth/login';
 export const REGISTER_ENDPOINT = '/social/auth/register';
 export const POSTS_ENDPOINT = '/social/posts/';
+export const PROFILES_ENDPOINT = '/social/profiles/';
