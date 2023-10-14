a32a4c87a6985deba9f69cd22bd841130d4fed80
hyseh <edgyhyseh@gmail.com>
2023-10-14 20:06:46 +0200

    added sort to function

--- js/utility/get.js
+++ js/utility/get.js
@@ -1,7 +1,7 @@
-export async function getPosts(url, endpoint, limit, offset, render) {
+export async function getPosts(url, endpoint, limit, offset, sort, render) {
   let tag = '&_tag=hyseh';
   let author = '&_author=true';
-  let param = `?limit=${limit}&offset=${offset}${tag}${author}`;
+  let param = `?limit=${limit}&offset=${offset}&sortOrder=${sort}${tag}${author}`;
   try {
     const token = localStorage.getItem('token');
     const res = await fetch(url + endpoint + param, {
@@ -36,6 +36,28 @@ export async function getPostSpecific(url, endpoint, id, render, update) {
     console.log(data);
     render(data);
     update(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getProfile(url, endpoint, id, render) {
+  let author = '?_author=true';
+  let posts = '?_posts=true';
+  let param = `${posts}`;
+
+  try {
+    const token = localStorage.getItem('token');
+    const res = await fetch(url + endpoint + id + param, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'Application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const data = await res.json();
+    console.log(data);
+    render(data);
   } catch (error) {
     console.log(error);
   }
